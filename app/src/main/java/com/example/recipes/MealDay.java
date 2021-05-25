8369cd32205315e2e357f7de2f2edbf933669b99
ceyda-albayrak <ceydaa2098@gmail.com>
2021-05-26 00:44:51 +0300

    refactor: some faulty page designs revised

--- app/src/main/java/com/example/recipes/MealDay.java
+++ app/src/main/java/com/example/recipes/MealDay.java
@@ -11,6 +11,8 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -99,6 +101,17 @@ public class MealDay extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 HashMap hm= list.get(position);
+                food_details foodDetails = new food_details();
+                Bundle args = new Bundle();
+                args.putString("Title", hm.get("Title").toString() ) ;
+                args.putString("Recipe", hm.get("Recipe").toString() ) ;
+                args.putString("Description", hm.get("Description").toString() ) ;
+                args.putString("ImagePath", hm.get("ImagePath").toString() ) ;
+                foodDetails.setArguments(args);
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.content, foodDetails);
+                transaction.commit();
             }
         });
         return v;
